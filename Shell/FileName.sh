20b19ea4ca6da9fa95fb0700e83474695f00f1ca
levenwindy <winyiday@qq.com>
2023-05-25 18:48:11 +0800

    Update FileName.sh

--- Shell/FileName.sh
+++ Shell/FileName.sh
@@ -3,20 +3,20 @@
 NUM=$1
 GITHUB_ENV=$2
 
-export FILENAME=$SERIAL'.$NUM-360T7-'$NAME'-108M'
-export BIN=$SERIAL'.$NUM-360T7-'$NAME'-108M-BIN'
-export FACTORY=$SERIAL'.$NUM-360T7-'$NAME'-108M-squashfs-factory'
-export SYSUPGRADE=$SERIAL'.$NUM-360T7-'$NAME'-108M-squashfs-sysupgrade'
+export FILENAME=$SERIAL'.'$NUM'-360T7-'$NAME'-108M'
+export BIN=$SERIAL'.'$NUM'-360T7-'$NAME'-108M-BIN'
+export FACTORY=$SERIAL'.'$NUM'-360T7-'$NAME'-108M-squashfs-factory'
+export SYSUPGRADE=$SERIAL'.'$NUM'-360T7-'$NAME'-108M-squashfs-sysupgrade'
 
 echo "FILENAME=$FILENAME" >> $GITHUB_ENV
 echo "BIN=$BIN" >> $GITHUB_ENV
 echo "FACTORY=$FACTORY" >> $GITHUB_ENV
 echo "SYSUPGRADE=$SYSUPGRADE" >> $GITHUB_ENV
 
-export VanillaFILENAME=$SERIAL'.$NUM-360T7-'$NAME
-export VanillaBIN=$SERIAL'.$NUM-360T7-'$NAME'-BIN'
-export VanillaFACTORY=$SERIAL'.$NUM-360T7-'$NAME'-squashfs-factory'
-export VanillaSYSUPGRADE=$SERIAL'.$NUM-360T7-'$NAME'-squashfs-sysupgrade'
+export VanillaFILENAME=$SERIAL'.'$NUM'-360T7-'$NAME
+export VanillaBIN=$SERIAL'.'$NUM'-360T7-'$NAME'-BIN'
+export VanillaFACTORY=$SERIAL'.'$NUM'-360T7-'$NAME'-squashfs-factory'
+export VanillaSYSUPGRADE=$SERIAL'.'$NUM'-360T7-'$NAME'-squashfs-sysupgrade'
 
 echo "VanillaFILENAME=$VanillaFILENAME" >> $GITHUB_ENV
 echo "VanillaBIN=$VanillaBIN" >> $GITHUB_ENV
